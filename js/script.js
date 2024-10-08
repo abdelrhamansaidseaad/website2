5a53820bad0ef78b51d3f4a7a739ac48fde4091e
Abdulrahman Saeed Saad <144129821+abdelrhamansaidseaad@users.noreply.github.com>
2024-10-08 04:06:41 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+//  var productbtn = document.getElementById("productbtn")
+//  var productCountainer = document.getElementById("productCountainer")
+
+//  data =[
+//   {id:1 , name : "abdo" , dec :"abdo loves rana"},
+//   {id:2 , name : "manal" , dec :"abdo loves sara"},
+//   {id:3 , name : "ahmed" , dec :"abdo loves mona"},
+//   {id:4 , name : "walid" , dec :"abdo loves aya"}
+// ]
+// function deleteItem(){}
+// function Draw(items)
+// {
+//   productCountainer.innerHTML = "";
+//   items.forEach (function(element){
+//  productCountainer.innerHTML+=  <div onclick = deleteItem ( $ { element.name} ) > + $ { element.name } + </div> 
+// })
+// }
+// window.onload=function()
+// {
+//   Draw(data);
+// }
+
+// productbtn.addEventListener("click" , addItem);
+
+//  function addItem()
+// {
+//   var productInput = document.getElementById("productInput")
+//   if(productInput.value==""){
+//   alert("you should enter a product)
+//   }
+//   function deleteItem(){};
+//   var lastId = data[data.length-1].id;
+//   data.push({id : ++lastId , name : "jjjj" ,dec : ".sssssssssssssss......." })
+
+//   var addedItem = data[data.length-1];
+//   productCountainer.innerHTML +=  <div onclick = deleteItem( ${addedItem.id}) >+ ${addedItemname}</div> '
+//   productInput.value="";
+
+// } 
+//  function deleteItem()
+//  {
+//       var index = data.map (function(im) {
+//         return im.id 
+//       }).indexOf(id)
+
+//      if(index ==-1)
+//      {
+//       data.splice(index , 1);
+//       Draw(data);
+//      }  
+//      console.log(data)
+//  }
+
